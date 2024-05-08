109e11a1a62190bdd1e482fd7194793cddf692f0
LynStevenson <81272840+LynStevenson@users.noreply.github.com>
2024-05-07 19:15:00 -0500

    add game logic

--- script.js
+++ script.js
@@ -23,3 +23,34 @@ function getHumanChoice(){
     } while(humanChoice !== 'rock' && humanChoice !== 'paper' && humanChoice !== 'scissors');
     return humanChoice;
 }
+
+function playGame(){
+    let humanScore = 0;
+    let botScore = 0;
+    function playRound(humanChoice, botChoice){
+        if (humanChoice === botChoice){
+            console.log("It's a tie! Both players selected " + humanChoice);
+            return;
+        } else if (humanChoice === "rock" && botChoice === "scissors"
+                    || humanChoice === "scissors" && botChoice === "paper"
+                    || humanChoice === "paper" && botChoice === "rock"){
+            console.log("You win! " + humanChoice + " beats " + botChoice);
+            humanScore++;
+            return;
+        } else if (humanChoice === "rock" && botChoice === "paper"
+                    || humanChoice === "scissors" && botChoice === "rock"
+                    || humanChoice === "paper" && botChoice === "scissors"){
+            console.log("You lose! " + humanChoice + " lost to " + botChoice);
+            botScore++;
+            return;
+        } console.log("logic error in playRound");
+    }
+    for (let i = 1; i<=5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore > botScore){
+        console.log("Congrats! You won Rock Paper Scissors!");
+    } else {
+        console.log("The computer won Rock Paper Scissors! Try again!");
+    }
+}
